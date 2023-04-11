7dd4dc78287fc9f924ebaf3465b9bd37fd4417ad
jlotero23 <125835077+jlotero23@users.noreply.github.com>
2023-04-11 10:40:26 -0500

    Intento namber guan

---
+++ src/main/java/comunicacion/Tesis.java
@@ -0,0 +1,66 @@
+package comunicacion;
+
+public class Tesis extends Escrito {
+	
+	private String idea;
+	private String[] argumentos;
+	private String conclusion;
+	private String referencias;
+	private String interpretacion;
+	
+	public Tesis(String origen, String titulo, String autor, int paginas, String idea, String[] argumentos, String conclusion,String referencias, String interpretacion) {
+		super(origen, titulo, autor, paginas);
+		this.idea=idea;
+		this.argumentos=argumentos;
+		this.conclusion=conclusion;
+		this.referencias=referencias;
+		this.interpretacion=interpretacion;
+	}
+	
+	public int palabrasTotales(int palabras) {
+		return (super.getPaginas())*5;
+	}
+	public String interpretacion() {
+		return interpretacion;
+	}
+	public String toString() {
+		return  super.getOrigen() +"\n"+
+				super.getTitulo() +"\n"+
+				super.getAutor() +"\n"+
+				super.getPaginas()+"\n"+
+				getIdea() +"\n"+
+				argumentos.length +"\n"+
+				getConclusion() +"\n"+
+				getReferencias()+"\n";
+	}
+	public String getIdea() {
+		return idea;
+	}
+	public void setIdea(String idea) {
+		this.idea = idea;
+	}
+	public String[] getArgumentos() {
+		return argumentos;
+	}
+	public void setArgumentos(String[] argumentos) {
+		this.argumentos = argumentos;
+	}
+	public String getReferencias() {
+		return referencias;
+	}
+	public void setReferencias(String referencias) {
+		this.referencias = referencias;
+	}
+	public String getConclusion() {
+		return conclusion;
+	}
+	public void setConclusion(String conclusion) {
+		this.conclusion = conclusion;
+	}
+	public String getInterpretacion() {
+		return interpretacion;
+	}
+	public void setInterpretacion(String interpretacion) {
+		this.interpretacion = interpretacion;
+	}
+}
